f6d0fa84c51a8ad444099cc838623300755b311a
Jackson Ming Hu <huming2207@gmail.com>
2019-07-22 22:30:52 +1000

    ws_client: implement base client

--- client/include/ws_client.hpp
+++ client/include/ws_client.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <functional>
+
 namespace snet::client
 {
     class ws_client
     {
+        protected:
+            std::function<void()> on_connect_cb;
+            std::function<void()> on_disconnect_cb;
+            std::function<void(int)> on_error_cb;
+            std::function<void(const char *, int)> on_receive_cb;
 
+        public:
+        virtual void set_auth(const std::string& user_name, const std::string& password) = 0;
+        virtual void set_cert_pem(const std::string& pem) = 0;
+        virtual int connect() = 0;
+        virtual void on_connect_change(const std::function<void()>& connect_cb, const std::function<void()>& disconnect_cb) = 0;
+        virtual void on_receive(const std::function<void(const char *, int)>& cb) = 0;
+        virtual void on_error(const std::function<void(int)>& cb) = 0;
     };
 }
--- client/platform/include/esp_ws_client.hpp
+++ client/platform/include/esp_ws_client.hpp
@@ -19,21 +19,17 @@ namespace snet::client
         public:
             explicit esp_ws_client(const std::string& uri);
             esp_ws_client(const std::string& host, uint16_t port, const std::string& path);
-            void set_auth(const std::string& user_name, const std::string& password);
-            void set_cert_pem(const std::string& pem);
-            int connect();
-            void on_connect_change(const std::function<void()>& connect_cb = {},
-                                    const std::function<void()>& disconnect_cb = {});
-            void on_receive(const std::function<void(const char *, int)>& cb);
-            void on_error(const std::function<void(int)>& cb);
+            void set_auth(const std::string& user_name, const std::string& password) override;
+            void set_cert_pem(const std::string& pem) override;
+            int connect() override;
+            void on_connect_change(const std::function<void()>& connect_cb ,
+                                    const std::function<void()>& disconnect_cb) override;
+            void on_receive(const std::function<void(const char *, int)>& cb) override;
+            void on_error(const std::function<void(int)>& cb) override;
 
         private:
             esp_websocket_client_config_t config{};
             esp_websocket_client_handle_t handle = nullptr;
-            std::function<void()> on_connect_cb;
-            std::function<void()> on_disconnect_cb;
-            std::function<void(int)> on_error_cb;
-            std::function<void(const char *, int)> on_receive_cb;
             EventGroupHandle_t ws_event;
     };
 }

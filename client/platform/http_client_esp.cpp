7a9c07ee90f6f8bfaa24b8fcb804af886c3c0738
Jackson Ming Hu <huming2207@gmail.com>
2019-07-25 23:13:38 +1000

    http_client: method init

--- client/platform/http_client_esp.cpp
+++ client/platform/http_client_esp.cpp
@@ -1,16 +1,115 @@
+#include <sstream>
 #include "http_client_esp.hpp"
 
-snet::client::http_client_esp::http_client_esp(const std::string &url)
+using namespace snet::client;
+
+http_client_esp::http_client_esp(const std::string &url)
 {
     config.url = url.c_str();
     config.user_data = this;
     config.event_handler = &http_client_esp::http_event_handler;
-    esp_http_client_init(&config);
+    client = esp_http_client_init(&config);
 }
 
-esp_err_t snet::client::http_client_esp::http_event_handler(esp_http_client_event_t *event)
+http_client_esp::http_client_esp(const std::string &host, const std::string &path)
+{
+    config.host = host.c_str();
+    config.path = path.c_str();
+    config.event_handler = &http_client_esp::http_event_handler;
+    client = esp_http_client_init(&config);
+}
+
+
+
+esp_err_t http_client_esp::http_event_handler(esp_http_client_event_t *event)
 {
     auto client = static_cast<http_client_esp *>(event->user_data);
 
     return 0;
 }
+
+http_client_esp &http_client_esp::run_get()
+{
+    auto ret = esp_http_client_set_method(client, HTTP_METHOD_GET);
+    ret = ret ?: esp_http_client_perform(client);
+    if(ret != ESP_OK) on_error_cb(ret);
+    return *this;
+}
+
+http_client_esp &http_client_esp::run_post(const std::map<std::string, std::string>& field)
+{
+
+
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_put(const std::map<std::string, std::string>& field)
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_patch(const std::map<std::string, std::string>& field)
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_post(const std::vector<std::tuple<std::string, std::string>> &field)
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_put(const std::vector<std::tuple<std::string, std::string>> &field)
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_patch(const std::vector<std::tuple<std::string, std::string>> &field)
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::run_delete()
+{
+    return *this;
+}
+
+http_client_esp& http_client_esp::on_error(const std::function<void(int)>& cb)
+{
+    on_error_cb = cb;
+    return *this;
+}
+
+std::string http_client_esp::make_field(const std::map<std::string, std::string> &field)
+{
+    // Make it to a post field string
+    std::stringstream post_field;
+    for(auto& item : field) {
+        post_field << item.first << "=" << item.second << "&";
+    }
+
+    // Make it a string
+    auto field_str = post_field.str();
+
+    // Remove the last '&'
+    field_str.pop_back();
+
+    return field_str;
+}
+
+std::string http_client_esp::make_field(const std::vector<std::tuple<std::string, std::string>> &field)
+{
+    // Make it to a post field string
+    std::stringstream post_field;
+    for(auto& item : field) {
+        post_field << std::get<0>(item) << "=" << std::get<1>(item) << "&";
+    }
+
+    // Make it a string
+    auto field_str = post_field.str();
+
+    // Remove the last '&'
+    field_str.pop_back();
+
+    return field_str;
+}
+
--- client/platform/include/http_client_esp.hpp
+++ client/platform/include/http_client_esp.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <string_view>
+#include <functional>
 #include <esp_http_client.h>
 
 namespace snet::client
@@ -11,10 +12,24 @@ namespace snet::client
     {
         public:
             explicit http_client_esp(const std::string& url);
-            http_client_esp() : http_client_esp("") {};
+            http_client_esp(const std::string& host, const std::string& path);
+            http_client_esp& on_error(const std::function<void(int)>& cb);
+            http_client_esp& run_get();
+            http_client_esp& run_post(const std::map<std::string, std::string>& field);
+            http_client_esp& run_put(const std::map<std::string, std::string>& field);
+            http_client_esp& run_patch(const std::map<std::string, std::string>& field);
+            http_client_esp& run_post(const std::vector<std::tuple<std::string, std::string>>& field);
+            http_client_esp& run_put(const std::vector<std::tuple<std::string, std::string>>& field);
+            http_client_esp& run_patch(const std::vector<std::tuple<std::string, std::string>>& field);
+            http_client_esp& run_delete();
 
         private:
             esp_http_client_config_t config{};
+            esp_http_client_handle_t client = nullptr;
+            std::function<void(int)> on_error_cb;
+
+            std::string make_field(const std::map<std::string, std::string>& field);
+            std::string make_field(const std::vector<std::tuple<std::string, std::string>>& field);
             static esp_err_t http_event_handler(esp_http_client_event_t *event);
     };
 }
